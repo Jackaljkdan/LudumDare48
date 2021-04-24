e7473dae0312decff68094ade2f8409e91fd765a
Daniele Cortesi <dani.c93@hotmail.it>
2021-04-24 20:40:52 +0200

    Add some utils

---
+++ Assets/JK/Utils/Events.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace JK.Utils
+{
+    [Serializable]
+    public class UnityEventVector3 : UnityEvent<Vector3> { }
+}
